ba6fac09f04799ecc208ad447b6233618bf3db50
OfferComer <124803189+OfferComer@users.noreply.github.com>
2022-09-21 15:13:07 +0000

    perf: protect group_leader from races that cause ctx double-free

--- cmd/config.go
+++ cmd/config.go
@@ -11,3 +11,18 @@ import (
 // configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
+	Short: "Display default config",
+	Run: func(cmd *cobra.Command, args []string) {
+		showExample, _ := cmd.Flags().GetBool("example")
+		if showExample {
+			fmt.Println(config.ExampleConfig())
+		} else {
+			fmt.Println(config.DefaultConfig())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+	configCmd.Flags().BoolP("example", "e", false, "Display example config")
+}
---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	cfgFile string
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "PAL9000",
+	Short: "PAL9000 is a tool to connect to botastic APIs",
+	Long:  `With pal9000, you can easily deploy your own bot application using botastic`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.yaml", "config file (default is config.yaml)")
