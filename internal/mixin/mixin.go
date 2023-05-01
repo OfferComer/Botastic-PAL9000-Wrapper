79f7fa066d6f2026768ae9d2a745bf219ea2419f
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-05-01 09:21:21 +0000

    added error message

--- internal/mixin/mixin.go
+++ internal/mixin/mixin.go
@@ -12,4 +12,5 @@ import (
 	"github.com/fox-one/mixin-sdk-go"
 	"github.com/fox-one/pkg/uuid"
 	"github.com/pandodao/PAL9000/config"
-	"github.com/pan
+	"github.com/pandodao/PAL9000/service"
+	"github.com
