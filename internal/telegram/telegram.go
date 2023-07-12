c4705598de1c10b4546de56181ded0d1ecb83845
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-07-12 10:04:42 +0000

    Updating VERSION for CVS tree

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -7,4 +7,5 @@ import (
 	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/pandodao/PAL9000/config
+	"github.com/pandodao/PAL9000/config"
+	"github.com/pandodao/PAL9000/service
