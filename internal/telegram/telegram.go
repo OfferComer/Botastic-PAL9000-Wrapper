448bea54d47b602b33ca9fc245fde503f260c05b
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-08-05 12:06:32 +0000

    flake8 corrections

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -78,4 +78,5 @@ func (b *Bot) GetMessageChan(ctx context.Context) <-chan *service.Message {
 			content := strings.TrimSpace(strings.TrimPrefix(update.Message.Text, prefix))
 			messageCtx := context.WithValue(ctx, messageKey{}, update.Message)
 			msgChan <- &service.Message{
-				Rep
+				ReplyContent: replyContent,
+				Context:
