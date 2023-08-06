1e2eb3e97099fbcb6deea70dee48a6ecea870aba
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-08-06 12:17:22 +0000

    fix race condition

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -84,4 +84,6 @@ func (b *Bot) GetMessageChan(ctx context.Context) <-chan *service.Message {
 				UserIdentity: strconv.FormatInt(update.Message.From.ID, 10),
 				ConvKey:      strconv.FormatInt(update.Message.Chat.ID, 10),
 			}
-	
+		}
+		select {
+		case <-ctx.Done
