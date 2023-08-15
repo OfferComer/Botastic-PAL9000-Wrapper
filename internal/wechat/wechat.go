70edc3c0fe5c63dac29a0d13703890988efcdaa8
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-08-15 21:44:09 +0000

    Additions, plus:

--- internal/wechat/wechat.go
+++ internal/wechat/wechat.go
@@ -3,3 +3,36 @@ package wechat
 
 import (
 	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/pandodao/PAL9000/config"
+	"github.com/pandodao/PAL9000/service"
+)
+
+type httpRequsetKey struct{}
+type httpResponseKey struct{}
+type rawMessageKey struct{}
+
+type TextMessage struct {
+	XMLName      xml.Name `xml:"xml"`
+	ToUserName   string   `xml:"ToUserName"`
+	FromUserName string   `xml:"FromUserName"`
+	CreateTime   int64    `xml:"CreateTime"`
+	MsgType      string   `xml:"MsgType"`
+	Content      string   `xml:"Content"`
+	MsgId        int64    `xml:"MsgId"`
+}
+
+type Bot struct {
+	name string
+	cfg  config.WeChatConfig
+}
