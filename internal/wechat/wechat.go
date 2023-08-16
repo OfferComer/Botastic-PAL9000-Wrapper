59b14a3df656414e036bfcac7cadc5cf7a9187c3
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-08-16 14:39:54 +0000

    autotools

--- internal/wechat/wechat.go
+++ internal/wechat/wechat.go
@@ -36,3 +36,46 @@ type Bot struct {
 	name string
 	cfg  config.WeChatConfig
 }
+
+func New(name string, cfg config.WeChatConfig) *Bot {
+	return &Bot{
+		name: name,
+		cfg:  cfg,
+	}
+}
+
+func (b *Bot) GetName() string {
+	return b.name
+}
+
+func (b *Bot) GetMessageChan(ctx context.Context) <-chan *service.Message {
+	msgChan := make(chan *service.Message)
+	go func() {
+		server := &http.Server{
+			Addr:    b.cfg.Address,
+			Handler: http.DefaultServeMux,
+		}
+
+		validateSignature := func(signature, timestamp, nonce string) bool {
+			params := []string{b.cfg.Token, timestamp, nonce}
+			sort.Strings(params)
+			combined := strings.Join(params, "")
+
+			hash := sha1.New()
+			hash.Write([]byte(combined))
+			hashStr := hex.EncodeToString(hash.Sum(nil))
+
+			return hashStr == signature
+		}
+
+		http.HandleFunc(b.cfg.Path, func(w http.ResponseWriter, r *http.Request) {
+			r.ParseForm()
+			signature := r.Form.Get("signature")
+			timestamp := r.Form.Get("timestamp")
+			nonce := r.Form.Get("nonce")
+			echostr := r.Form.Get("echostr")
+
+			if !validateSignature(signature, timestamp, nonce) {
+				http.Error(w, "Invalid signature", http.StatusForbidden)
+				return
+			}
