9d54f5ccdb67163c0357eba48fc934e584032733
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-08-22 12:31:38 +0000

    PARISC: fix user-triggerable panic on parisc

--- service/service.go
+++ service/service.go
@@ -13,4 +13,8 @@ import (
 )
 
 var (
-	linkRegex = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=
+	linkRegex = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
+)
+
+type Adapter interface {
+	GetName() 
