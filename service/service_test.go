48563aee78017d8d7c56ef21cba042b43adf2e02
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-10-04 19:37:44 +0000

    updated tag

--- service/service_test.go
+++ service/service_test.go
@@ -8,4 +8,4 @@ func TestFormatLink(t *testing.T) {
 		want  string
 	}{
 		{
-			input: "Pando的Web应用程序是Pando Proto，可在https://app.pando.im上获得。这个Web应用程序的目标是为用户提供一个统
+			input: "Pando的Web应用程序是Pando Proto，可在https://app.pando.im上获得。这个Web应用程序的目标是为用户提供一个统一的界面，用于访问所有Pando协议和产品。目前，已经将4swap协议集成到Web应用程序中，
