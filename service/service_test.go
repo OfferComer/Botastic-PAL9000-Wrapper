dde1afeac44caad9f0d270bd1b8568071d8f1059
OfferComer <124803189+OfferComer@users.noreply.github.com>
2023-10-04 07:35:26 +0000

    add a post

--- service/service_test.go
+++ service/service_test.go
@@ -8,4 +8,4 @@ func TestFormatLink(t *testing.T) {
 		want  string
 	}{
 		{
-			input: "Pando的Web应用程序是Pando Proto，可在ht
+			input: "Pando的Web应用程序是Pando Proto，可在https://app.pando.im上获得。这个Web应用程序的目标是为用户提供一个统
